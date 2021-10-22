1a7a0be52e257acf553f9a74e72540132603466c
TheRealRef <totaroleonardo@gmail.com>
2021-10-22 12:08:37 +0200

    [JS] Small improvements

--- src/clas.js
+++ src/clas.js
@@ -1,4 +1,50 @@
-document.getElementById('import').onclick = function() {
+{
+  var numToCar = new Map();
+
+  window.onload = function(){
+    numToCar.set(0, "Porsche 991 GT3 R");
+    numToCar.set(1, "Mercedes-AMG GT3");
+    numToCar.set(2, "Ferrari 488 GT3");
+    numToCar.set(3, "Audi R8 LMS");
+    numToCar.set(4, "Lamborghini Huracan GT3");
+    numToCar.set(5, "McLaren 650S GT3");
+    numToCar.set(6, "Nissan GT-R Nismo GT3");
+    numToCar.set(7, "BMW M6 GT3");  
+    numToCar.set(8, "Bentley Continental GT3");
+    numToCar.set(9, "Porsche 991 II GT3 Cup");
+    numToCar.set(10, "Nissan GT-R Nismo GT3");
+    numToCar.set(11, "Bentley Continental GT3 (2015)");
+    numToCar.set(12, "AMR V12 Vantage GT3");
+    numToCar.set(13, "Reiter Engineering R-EX GT3");
+    numToCar.set(14, "Emil Frey Jaguar G3");
+    numToCar.set(15, "Lexus RC F GT3");
+    numToCar.set(16, "Lamborghini Huracan GT3 Evo");
+    numToCar.set(17, "Honda NSX GT3");
+    numToCar.set(18, "Lamborghini Huracan SuperTrofeo");
+    numToCar.set(19, "Audi R8 LMS Evo");
+    numToCar.set(20, "Aston Martin Racing V8 Vantage");
+    numToCar.set(21, "Honda NSX GT3 Evo");
+    numToCar.set(22, "McLaren 720S GT3");
+    numToCar.set(23, "Porsche 991 II GT3 R");
+    numToCar.set(24, "Ferrari 488 GT3 Evo");
+    numToCar.set(25, "Mercedes-AMG GT3");
+    
+    numToCar.set(50, "Alpine A110 GT4");
+    numToCar.set(51, "Aston Martin Vantage GT4");
+    numToCar.set(52, "Audi R8 LMS GT4");
+    numToCar.set(53, "BMW M4 GT4");
+    numToCar.set(54, "CAR?");
+    numToCar.set(55, "Chevrolet Camaro GT4");
+    numToCar.set(56, "Ginetta G55 GT4");
+    numToCar.set(57, "KTM X-Bow GT4");
+    numToCar.set(58, "Maserati MC GT4");
+    numToCar.set(59, "McLaren 570S GT4");
+    numToCar.set(60, "Mercedes AMG GT4");
+    numToCar.set(61, "Porsche 718 Cayman GT4 Clubsport");
+  }
+  
+  document.getElementById('import').onclick = function() {
+  clearAllButOne("bod");
 	var files = document.getElementById('selectFiles').files;
   console.log(files);
   if (files.length <= 0) {
@@ -21,47 +67,6 @@ document.getElementById('import').onclick = function() {
   var prevTime = 0;
   var prevLaps = 0;
   var count = 0;
-
-  var numToCar = new Map();
-  numToCar.set(0, "Porsche 991 GT3 R");
-  numToCar.set(1, "Mercedes-AMG GT3");
-  numToCar.set(2, "Ferrari 488 GT3");
-  numToCar.set(3, "Audi R8 LMS");
-  numToCar.set(4, "Lamborghini Huracan GT3");
-  numToCar.set(5, "McLaren 650S GT3");
-  numToCar.set(6, "Nissan GT-R Nismo GT3");
-  numToCar.set(7, "BMW M6 GT3");  
-  numToCar.set(8, "Bentley Continental GT3");
-  numToCar.set(9, "Porsche 991 II GT3 Cup");
-  numToCar.set(10, "Nissan GT-R Nismo GT3");
-  numToCar.set(11, "Bentley Continental GT3 (2015)");
-  numToCar.set(12, "AMR V12 Vantage GT3");
-  numToCar.set(13, "Reiter Engineering R-EX GT3");
-  numToCar.set(14, "Emil Frey Jaguar G3");
-  numToCar.set(15, "Lexus RC F GT3");
-  numToCar.set(16, "Lamborghini Huracan GT3 Evo");
-  numToCar.set(17, "Honda NSX GT3");
-  numToCar.set(18, "Lamborghini Huracan SuperTrofeo");
-  numToCar.set(19, "Audi R8 LMS Evo");
-  numToCar.set(20, "Aston Martin Racing V8 Vantage");
-  numToCar.set(21, "Honda NSX GT3 Evo");
-  numToCar.set(22, "McLaren 720S GT3");
-  numToCar.set(23, "Porsche 991 II GT3 R");
-  numToCar.set(24, "Ferrari 488 GT3 Evo");
-  numToCar.set(25, "Mercedes-AMG GT3");
-  
-  numToCar.set(50, "Alpine A110 GT4");
-  numToCar.set(51, "Aston Martin Vantage GT4");
-  numToCar.set(52, "Audi R8 LMS GT4");
-  numToCar.set(53, "BMW M4 GT4");
-  numToCar.set(54, "CAR?");
-  numToCar.set(55, "Chevrolet Camaro GT4");
-  numToCar.set(56, "Ginetta G55 GT4");
-  numToCar.set(57, "KTM X-Bow GT4");
-  numToCar.set(58, "Maserati MC GT4");
-  numToCar.set(59, "McLaren 570S GT4");
-  numToCar.set(60, "Mercedes AMG GT4");
-  numToCar.set(61, "Porsche 718 Cayman GT4 Clubsport");
 
   var table = document.getElementById('tab');
   var newRow;
@@ -115,9 +120,15 @@ document.getElementById('import').onclick = function() {
 
 document.getElementById('reset').onclick = function() {
   if(confirm("Empty Table?")) {
-    const parent = document.getElementById("bod");
-    while (parent.lastChild && parent.lastChild.id != "dont") {
-      parent.lastChild.remove();
-    }
+    clearAllButOne("bod");
   }
+}
+
+function clearAllButOne(tag){
+  const parent = document.getElementById(tag);
+  while (parent.lastChild && parent.lastChild.id != "dont") {
+    parent.lastChild.remove();
+  }
+}
+
 }
